5966c794c2a1fa47a9f3c12037ba91dd265c0a44
Richard Allen <rsaxvc@gmail.com>
2012-08-22 22:09:34 -0500

    add instruction-count when emulator completes

--- main.c
+++ main.c
@@ -8,6 +8,8 @@
 
 int main(int numArgs, const char * args[] )
 {
+int inscnt = 0;
+
 if( numArgs != 2 )
 	{
 	printf("usage: %s initmemfile\n", args[0] );
@@ -18,9 +20,11 @@ load_filename( args[1] );
 
 while( instruction_step() )
 	{
+	inscnt++;
 	}
+inscnt++;
 
 printf("machine exit due to:%s\n", vm_err_str );
-
+printf("executed %i instructions\n", inscnt );
 return 5;
 }
